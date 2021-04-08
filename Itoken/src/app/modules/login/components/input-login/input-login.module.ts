5cb758ef98e47471a767c696d0d2889a7f514886
Davi Batista <DaviBatista.gb@gmail.com>
2021-04-08 17:19:15 -0300

    correcao bug angular

--- Itoken/src/app/modules/login/components/input-login/input-login.module.ts
+++ Itoken/src/app/modules/login/components/input-login/input-login.module.ts
@@ -1,37 +1,29 @@
-import { InputLoginComponent } from './input-login.component';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatCardModule} from '@angular/material/card';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs';
 import { ButtonModule } from 'src/app/ui/button/button.module';
-
-
-
+import { InputLoginComponent } from './input-login.component';
 
 @NgModule({
-  declarations: [
-    InputLoginComponent,
-  ],
+  declarations: [InputLoginComponent],
   imports: [
     CommonModule,
     MatFormFieldModule,
-    MatCardModule, 
-    MatTabsModule, 
+    MatCardModule,
+    MatTabsModule,
     MatIconModule,
     MatCheckboxModule,
     MatInputModule,
     ReactiveFormsModule,
-    ButtonModule
+    ButtonModule,
   ],
 
-  exports: [InputLoginComponent]
-  
+  exports: [InputLoginComponent],
 })
-export class InputLoginModule { }
+export class InputLoginModule {}
